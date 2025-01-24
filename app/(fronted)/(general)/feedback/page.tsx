3af437b36988df7ead58590f5a33848f949e7deb
JoonLi <jaydenlaijoonli@gmail.com>
2025-01-24 17:49:50 +0800

    commit -m "your commit message"

--- app/(fronted)/(general)/feedback/page.tsx
+++ app/(fronted)/(general)/feedback/page.tsx
@@ -1,6 +1,20 @@
+"use client";
 import React from "react";
 
 const Feedback = () => {
+const feedback = [
+  {
+    name: "John Doe",
+    email: "EMAIL",
+    message: "This is a test message.",
+  },
+  {
+    name: "Jane Smith",
+    email: "EMAIL",
+    message: "This is another test message.",
+  },
+]
+
   return <div>Feedback</div>;
 };
 
