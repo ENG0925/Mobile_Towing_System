3d34de460bc3ba8b29361ad4173708f38bb14b62
WeiJiun1234 <weijiun.kidydult@one2deal.xyz>
2025-01-25 13:08:59 +0800

    update gitigonre

--- lib/api/user.ts
+++ lib/api/user.ts
@@ -1,8 +1,70 @@
 import axios from "axios";
 
-export const insertSuperAdmin = async () => {
+interface ReqRegisterData {
+    account: {
+        confirmPassword: string;
+        contactNumber: string;
+        email: string;
+        password: string;
+        username: string;
+    };
+    policy: {
+        hasPolicy: boolean;
+        policyNo: string;
+        icNumber: string;
+        policyHolderName: string;
+        uploadFile: File;
+    };
+    vehicle: {
+        color: string;
+        plateNumber: string;
+        model: string;
+    };
+}
+
+export const checkUsernameAndEmail = async (username: string, email: string) => {
     try {
-        await axios.post('/mysql/seed/insertSuperAdmin');
+        await axios.post('/mysql/user/auth/checkUsernameAndEmail', { username, email });
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+export const getAccountDetail = async (id: number) => {
+    try {
+        await axios.post('/mysql/user/auth/getAccountDetail', { id });
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+export const login = async (username: string, password: string) => {
+    try {
+        await axios.post('/mysql/user/auth/login', { username, password });
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+export const registerAccount = async (data: ReqRegisterData) => {
+    try {
+        await axios.post('/mysql/user/auth/registerAccount', { data });
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+export const updatePassword = async (newPassword: string) => {
+    try {
+        await axios.post('/mysql/user/auth/updatePassword', { newPassword });
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+export const updateProfile = async (userName: string, email: string, contact: number) => {
+    try {
+        await axios.post('/mysql/user/auth/updateProfile', { userName, email, contact });
     } catch (error) {
         console.error("Error: ", error);
     }
