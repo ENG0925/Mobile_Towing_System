783b111ed5e2a4d6dec1f07104070f762b226067
JasonTan36 <tanjunxian36@gmail.com>
2025-02-10 21:08:11 +0800

    Build driver all-task-page, add option pending in system admin edit and create driver navbar

---
+++ app/(fronted)/driver/driver-all-task/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DriverAllTask = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-emerald-50 p-4">
+        <div>DriverAllTask</div>
+      </div>
+    </>
+  );
+};
+
+export default DriverAllTask;
--- app/(fronted)/system-admin/booking/page.tsx
+++ app/(fronted)/system-admin/booking/page.tsx
@@ -7,7 +7,15 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Button } from "@/components/ui/button";
 import SheetForm from "@/components/common/SheetForm";
-import { addBooking, getAllBooking, getAllDriverBooking, getAllUserBooking, getAllVehicleBooking, getBookingInfo, updateBooking } from "@/lib/api/system_admin";
+import {
+  addBooking,
+  getAllBooking,
+  getAllDriverBooking,
+  getAllUserBooking,
+  getAllVehicleBooking,
+  getBookingInfo,
+  updateBooking,
+} from "@/lib/api/system_admin";
 
 const Booking = () => {
   const [data, setData] = useState<booking[]>([]);
@@ -15,7 +23,7 @@ const Booking = () => {
   const [open, setOpen] = useState(false);
   const [selectedId, setSelectedId] = useState<number | null>(null);
   const [isEditing, setIsEditing] = useState(false);
-  const [key, setKey] = useState(0); 
+  const [key, setKey] = useState(0);
   const [userList, setUserList] = useState([]);
   const [vehicleList, setVehicleList] = useState([]);
   const [driverList, setDriverList] = useState([]);
@@ -35,7 +43,6 @@ const Booking = () => {
 
         const responseDriver = await getAllDriverBooking();
         setDriverList(responseDriver?.data);
-        
       } catch (error) {
         console.error("Error: ", error);
       } finally {
@@ -59,7 +66,7 @@ const Booking = () => {
   const handleDelete = async (id: number) => {
     try {
       console.log("Delete ID: ", id);
-      
+
       // const response = await deleteAdmin(id);
       // if (response?.data.success === true) {
       //   const responseData = await getAllAdmin();
@@ -84,14 +91,14 @@ const Booking = () => {
   const handlePassEdit = (id: number) => {
     setSelectedId(id);
     setIsEditing(true);
-    setKey(prevKey => prevKey + 1); // Increment key to force re-render
+    setKey((prevKey) => prevKey + 1); // Increment key to force re-render
     setOpen(true);
   };
 
   const handleAdd = () => {
     setSelectedId(null);
     setIsEditing(false);
-    setKey(prevKey => prevKey + 1); // Increment key to force re-render
+    setKey((prevKey) => prevKey + 1); // Increment key to force re-render
     setOpen(true);
   };
 
@@ -131,72 +138,90 @@ const Booking = () => {
   return (
     <>
       <SheetForm
-        key={key} 
+        key={key}
         open={open}
         setOpen={setOpen}
         title={isEditing ? "Edit Booking Detail" : "Add Booking"}
         description={isEditing ? "Edit booking detail" : "Add new booking"}
         fields={[
-          { 
-            label: "Name", 
-            type: "select", 
+          {
+            label: "Name",
+            type: "select",
             name: "userID",
-            options: userList.length === 0 ? [{ label: "No user found", value: 0 }] : userList.map((user: any) => ({
-              label: user.name,
-              value: user.id
-            }))
+            options:
+              userList.length === 0
+                ? [{ label: "No user found", value: 0 }]
+                : userList.map((user: any) => ({
+                    label: user.name,
+                    value: user.id,
+                  })),
           },
-          { 
-            label: "Vehicle", 
-            type: "select", 
+          {
+            label: "Vehicle",
+            type: "select",
             name: "vehicleID",
-            options: vehicleList.length === 0 ? [{ label: "No vehicle found", value: 0 }] : vehicleList.map((vehicle: any) => ({
-              label: `${vehicle.model}, ${vehicle.plateNumber}`,
-              value: vehicle.id
-            }))
+            options:
+              vehicleList.length === 0
+                ? [{ label: "No vehicle found", value: 0 }]
+                : vehicleList.map((vehicle: any) => ({
+                    label: `${vehicle.model}, ${vehicle.plateNumber}`,
+                    value: vehicle.id,
+                  })),
           },
-          { 
-            label: "Driver", 
-            type: "select", 
+          {
+            label: "Driver",
+            type: "select",
             name: "driverID",
-            options: driverList.length === 0 ? [{ label: "No driver found", value: 0 }] : driverList.map((driver: any) => ({
-              label: driver.name,
-              value: driver.id
-            }))
+            options:
+              driverList.length === 0
+                ? [{ label: "No driver found", value: 0 }]
+                : driverList.map((driver: any) => ({
+                    label: driver.name,
+                    value: driver.id,
+                  })),
           },
           { label: "Booking Date", type: "date", name: "bookingDate" },
-          { label: "Service Location", type: "description", name: "serviceLocation" },
-          { label: "Towing Location", type: "description", name: "towingLocation" },
+          {
+            label: "Service Location",
+            type: "description",
+            name: "serviceLocation",
+          },
+          {
+            label: "Towing Location",
+            type: "description",
+            name: "towingLocation",
+          },
           { label: "Distance", type: "number", name: "distance" },
           { label: "Estimate Cost", type: "number", name: "estimatedCost" },
-          { 
-            label: "Status", 
-            type: "select", 
+          {
+            label: "Status",
+            type: "select",
             name: "status",
-            options: [                        
-              { label: "Complete", value: "complete" },                                                                                                                                                
+            options: [
+              { label: "Complete", value: "complete" },
               { label: "Unpaid", value: "unpaid" },
               { label: "In Progress", value: "in-progress" },
-              { label: "Cancel", value: "cancel" }
-            ] 
-          }
+              { label: "Pending", value: "pending" },
+              { label: "Cancel", value: "cancel" },
+            ],
+          },
         ]}
         onSubmit={handleSubmit}
         fetchData={isEditing ? fetchAdminData : undefined}
         isLoading={loading}
       />
-      
+
       {loading && (
         <div className="flex items-center justify-center h-screen">
           <span className="ml-2 text-gray-500">Loading...</span>
         </div>
       )}
-      
+
       {!loading && (
         <div>
           <div className="">
             <Button onClick={handleAdd}>+ Add</Button>
-            
+
             <DataTable
               columns={columns({
                 handlePassEdit,
